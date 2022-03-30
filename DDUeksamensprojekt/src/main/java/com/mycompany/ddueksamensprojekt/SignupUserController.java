f5e644dd29821622c90d693fefe72dd9c364c86e
Rawlpluks <claramajse@gmail.com>
2022-03-30 10:32:51 +0200

    excel

--- DDUeksamensprojekt/src/main/java/com/mycompany/ddueksamensprojekt/SignupUserController.java
+++ DDUeksamensprojekt/src/main/java/com/mycompany/ddueksamensprojekt/SignupUserController.java
@@ -3,7 +3,7 @@ package com.mycompany.ddueksamensprojekt;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
-public class SecondaryController {
+public class SignupUserController {
 
     @FXML
     private void switchToPrimary() throws IOException {
