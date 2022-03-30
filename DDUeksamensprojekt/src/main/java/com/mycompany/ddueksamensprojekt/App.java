09ae5bba7438fa08f251de7b75bc30bf6b1b5310
Drulle19 <danielschmidtlarsen19@gmail.com>
2022-03-30 13:47:10 +0200

    ting

--- DDUeksamensprojekt/src/main/java/com/mycompany/ddueksamensprojekt/App.java
+++ DDUeksamensprojekt/src/main/java/com/mycompany/ddueksamensprojekt/App.java
@@ -8,6 +8,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import javafx.stage.Popup;
 
 /**
  * JavaFX App
@@ -16,25 +17,33 @@ public class App extends Application {
     
     private static User loggedInUser = new User();
     private static Scene scene;
+    private static Stage stage;
+    private static Popup popup;
 
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("loginUser"));
         stage.setScene(scene);
         stage.show();
+        
+        this.stage = stage;
     }
 
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
 
-    private static Parent loadFXML(String fxml) throws IOException {
+    static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
         launch();
+    }
+    
+    public static Stage getStage(){
+        return stage;
     }
 
     public static User getLoggedInUser() {
@@ -43,5 +52,21 @@ public class App extends Application {
 
     public static void setLoggedInUser(User loggedInUser) {
         App.loggedInUser = loggedInUser;
+    }
+    
+    public static void setPopup(Popup popup) {
+        App.popup = popup;
+    }
+
+    public static Popup getPopup() {
+        return popup;
+    }
+
+    public static void closePopup() {
+        popup.hide();
+    }
+
+    public static void openPopup() {
+        popup.show(stage);
     }
 }
