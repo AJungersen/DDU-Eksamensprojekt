b7a6a1d2095c0609517e88cd3cb9ae994e654971
Rawlpluks <claramajse@gmail.com>
2022-03-30 13:05:08 +0200

    wallet

--- DDUeksamensprojekt/src/main/java/com/mycompany/ddueksamensprojekt/App.java
+++ DDUeksamensprojekt/src/main/java/com/mycompany/ddueksamensprojekt/App.java
@@ -8,6 +8,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import javafx.stage.Popup;
 
 /**
  * JavaFX App
@@ -16,12 +17,16 @@ public class App extends Application {
     
     private static User loggedInUser = new User();
     private static Scene scene;
+    private static Stage stage;
+    private static Popup popup;
 
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("loginUser"));
         stage.setScene(scene);
         stage.show();
+        
+        this.stage = stage;
     }
 
     static void setRoot(String fxml) throws IOException {
@@ -43,5 +48,21 @@ public class App extends Application {
 
     public static void setLoggedInUser(User loggedInUser) {
         App.loggedInUser = loggedInUser;
+    }
+    
+    public static void setPopup(Popup popup) {
+        App.popup = popup;
+    }
+
+    public static Popup getPopup() {
+        return popup;
+    }
+
+    public static void closePopup() {
+        popup.hide();
+    }
+
+    public static void openPopup() {
+        popup.show(stage);
     }
 }
