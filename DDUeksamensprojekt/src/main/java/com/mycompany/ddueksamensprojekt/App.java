005f7a1da989ce3ba5fb48c2a210fe39bb611125
Drulle19 <danielschmidtlarsen19@gmail.com>
2022-04-01 13:07:38 +0200

    Update App.java

--- DDUeksamensprojekt/src/main/java/com/mycompany/ddueksamensprojekt/App.java
+++ DDUeksamensprojekt/src/main/java/com/mycompany/ddueksamensprojekt/App.java
@@ -22,7 +22,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("login"));
+        scene = new Scene(loadFXML("loginUser"));
         stage.setScene(scene);
         stage.show();
         
