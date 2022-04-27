d3aa1b4f1a2c1a42db74124fd9201e9f23187813
Drulle19 <danielschmidtlarsen19@gmail.com>
2022-04-27 14:04:11 +0200

    søgefunktion til varesider lavet

--- DDUeksamensprojekt/src/main/java/com/mycompany/ddueksamensprojekt/SubcatelogViewController.java
+++ DDUeksamensprojekt/src/main/java/com/mycompany/ddueksamensprojekt/SubcatelogViewController.java
@@ -4,10 +4,14 @@
  */
 package com.mycompany.ddueksamensprojekt;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.regex.Pattern;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 import repository.StoreLoadMethods;
 
 /**
@@ -16,12 +20,32 @@ import repository.StoreLoadMethods;
  * @author Clara Maj
  */
 public class SubcatelogViewController implements Initializable {
-
+    @FXML
+    private TextField userInput;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
     }    
-    
+    @FXML
+    public void toProfile() throws IOException{
+        App.setRoot("profile");
+    }
+    @FXML
+    public void toCart() throws IOException{
+        App.setRoot("cart");
+    }
+    @FXML
+    public void search(){
+        ArrayList<Product> allProducts = new ArrayList(); //senere skal dette udskiftes med databasekald
+        ArrayList<Product> products = new ArrayList();
+        String search = userInput.getText();
+        for(Product P: allProducts){
+            if(Pattern.matches(".*" + search + "+.*",P.getName()) == true){
+                products.add(P);
+            }
+        }
+        //Clara noget der smider alle produkter P ind i fxml
+    }
 }
