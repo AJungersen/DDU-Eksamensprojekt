21048515e97b7de57754861a0154506d0d38c941
Drulle19 <danielschmidtlarsen19@gmail.com>
2022-05-04 11:01:32 +0200

    Tilføj produkt til kurv

--- DDUeksamensprojekt/src/main/java/com/mycompany/ddueksamensprojekt/ProductInformationController.java
+++ DDUeksamensprojekt/src/main/java/com/mycompany/ddueksamensprojekt/ProductInformationController.java
@@ -4,12 +4,15 @@
  */
 package com.mycompany.ddueksamensprojekt;
 
+import static Classes.ProductCategory.SLIK_OG_SNACKS;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 /**
  * FXML Controller class
@@ -17,14 +20,22 @@ import javafx.scene.control.Slider;
  * @author Clara Maj
  */
 public class ProductInformationController implements Initializable {
-    Slider slider;
+    @FXML
+    TextField price;
+    @FXML
+    TextField stock;
+    Product product = new Product("produkt", 20, SLIK_OG_SNACKS);
+    @FXML
+    TextField numberOfProduct;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-        
+        //price.setText(Float.toString(App.currentProduct.getPrice()));
+        price.setText(Float.toString(product.getPrice()));
+        stock.setText("På lager");
+        //stock.setText((App.currentProduct.getStock()>0 ? "på lager":"ikke på lager"));
     }    
      @FXML
     private void openProfile() throws IOException {
@@ -40,6 +51,9 @@ public class ProductInformationController implements Initializable {
     }
     @FXML
     private void addToCart(){
-        App.currentCart.getProductsAsList().add(App.currentProduct);
+        for(int i = 0; i < Integer.parseInt(numberOfProduct.getText()); i++){
+            App.currentCart.getProductsAsList().add(product);
+            //App.currentCart.getProductsAsList().add(App.currentProduct);
+        }
     }
 }
