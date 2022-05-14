941ca59aa0eb770f76c9a356f26d8ed114139a61
stoftot <70943516+stoftot@users.noreply.github.com>
2022-05-14 20:02:39 +0200

    d

--- DDUeksamensprojekt/src/main/java/com/mycompany/ddueksamensprojekt/AdminProductViewController.java
+++ DDUeksamensprojekt/src/main/java/com/mycompany/ddueksamensprojekt/AdminProductViewController.java
@@ -1,8 +1,28 @@
 package com.mycompany.ddueksamensprojekt;
 
+import Classes.ProductCategory;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import java.util.regex.Pattern;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.VPos;
+import javafx.scene.Cursor;
+import javafx.scene.Node;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import repository.StoreDatabaseMethods;
+import repository.Tools;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -13,8 +33,198 @@ import javafx.scene.image.ImageView;
  *
  * @author chris
  */
-public class AdminProductViewController {
+public class AdminProductViewController implements Initializable{
+    private StoreDatabaseMethods sdm = new StoreDatabaseMethods();
+    private float boarderThikness = 2.5f;
+    private float textSpace = 5;
 
+    private float paneSpace_X = 10;
+    private float paneSpace_Y = 10;
+    private float paneSize_X = 137;
+    private float panesPerRow;
+
+    private float imgSize_X = paneSize_X - 2 * boarderThikness;
+    private float imgSize_Y = 143 - boarderThikness;
+
+    private float nameTextSize = 14;
+    private float namepos_X = 0;
+    private float namePos_Y = 158;
+
+    private float priceTextSize = 12;
+    private float pricePos_X = 0;
+    private float pricePos_Y = namePos_Y + nameTextSize;
+
+    private float lagerstatusPos_X = 0;
+    private float lagerstatusPos_Y = pricePos_Y + priceTextSize;
+    private float lagerstatusSize = 12;
+    private float lagerstatusCirkel_X = 0;
+    private float lagerstatusCirkel_Y = lagerstatusPos_Y;
+    private float lagerStatusCirkelSize = 7;
+
+    private float paneSize_Y = lagerstatusPos_Y + lagerstatusSize;
+    private Font nameFont = Font.font("italic", nameTextSize);
+    private Font priceFont = Font.font("italic", priceTextSize);
+
+    private int fewProductsRemaning = 10;
+    private ArrayList<Product> allProducts = new ArrayList<>();
+    private ArrayList<Product> curentProducts = new ArrayList<>();
+    
+    @FXML
+    private AnchorPane anchorPaneProducts;
+    @FXML
+    private TextField textFieldSearchBar;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        try {
+            //load allProducts
+            allProducts = sdm.getAllProducts();
+
+            //calc panes per row
+            panesPerRow = (int) Math.floor((anchorPaneProducts.getPrefWidth() - paneSpace_X) / (paneSize_X + paneSpace_X));
+
+            //update pane space x
+            paneSpace_X = (float) (anchorPaneProducts.getPrefWidth()
+                    - (paneSize_X * panesPerRow))
+                    / (2 + panesPerRow - 1);
+
+            //update anchor pane height
+            float newHeight = (float) ((paneSize_Y + paneSpace_Y)
+                    * Math.ceil(ProductCategory.values().length / panesPerRow) + paneSpace_Y);
+
+            anchorPaneProducts.setPrefHeight(newHeight);
+            loadProducts();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    private void loadProducts() {
+        anchorPaneProducts.getChildren().clear();
+
+        //insert allProducts
+        int column = 0;
+        int row = 1;
+
+        for (Product p : allProducts) {
+            //if (textFieldSearchBar.getText().isBlank() || (p.getName().toLowerCase().indexOf(textFieldSearchBar.getText().toLowerCase(), 0) == 0)) {
+            if (textFieldSearchBar.getText().isBlank()
+                    || (Pattern.matches(".*" + textFieldSearchBar.getText().toLowerCase() + "+.*", p.getName().toLowerCase()))) {
+
+                column++;
+                Pane pane = new Pane();
+
+                //Pane pane = new StackPane();
+                //pane pos
+                pane.setLayoutX((paneSpace_X * column) + (paneSize_X * (column - 1)));
+                pane.setLayoutY((paneSpace_Y * row) + (paneSize_Y * (row - 1)));
+
+                //pane size
+                pane.setMinSize(paneSize_X, paneSize_Y);
+                pane.setMaxSize(paneSize_X, paneSize_Y);
+
+                pane.setStyle("-fx-border-color: #666666;"
+                        + "-fx-border-width: " + boarderThikness + ";"
+                        + "-fx-background-color: #ffffff");
+
+                //product image
+                ImageView imgView = new ImageView(p.getImage());
+
+                imgView.setFitWidth(imgSize_X);
+                imgView.setFitHeight(imgSize_Y);
+
+                imgView.setLayoutX(boarderThikness);
+                imgView.setLayoutY(boarderThikness);
+
+                //name text
+                p.setName(Tools.capitalizeFirstLetter(p.getName()));
+                Text nameText = new Text(namepos_X, namePos_Y, p.getName());
+                nameText.setFont(nameFont);
+                nameText.setLayoutX(paneSize_X / 2 - nameText.getBoundsInLocal().getWidth() / 2);
+                //nameText.setTextOrigin(VPos.CENTER);
+
+                //price text
+                Text priceText = new Text(pricePos_X, pricePos_Y, Float.toString(p.getPrice()) + " DKK");
+                priceText.setFont(priceFont);
+                priceText.setLayoutX(paneSize_X / 2 - priceText.getBoundsInLocal().getWidth() / 2);
+                //priceText.setTextOrigin(VPos.CENTER);
+
+                //Lagerstatus text and circel
+                Text lagerstatusText = new Text(lagerstatusPos_X, lagerstatusPos_Y, "lagerstatus");
+                lagerstatusText.setFont(priceFont);
+                lagerstatusText.setLayoutX(paneSize_X / 2 - lagerstatusText.getBoundsInLocal().getWidth() / 2 - lagerStatusCirkelSize - 10);
+                lagerstatusText.setTextOrigin(VPos.CENTER);
+
+                Circle lagerstatusCircle = new Circle(lagerstatusCirkel_X, lagerstatusCirkel_Y, lagerStatusCirkelSize);
+                if (p.getStock() != 0) {
+                    if (p.getStock() <= fewProductsRemaning) {
+                        //few
+                        lagerstatusCircle.setFill(Paint.valueOf("#F7DC6F"));
+                    } else {
+                        //in stock
+                        lagerstatusCircle.setFill(Paint.valueOf("#82E0AA"));
+                    }
+                } else {
+                    //empty
+                    lagerstatusCircle.setFill(Paint.valueOf("#F1948A"));
+                    //lagerstatusCircle.setFill(Paint.valueOf("#82E0AA"));
+                    lagerstatusCircle.setFill(Paint.valueOf("#F7DC6F"));
+                }
+                lagerstatusCircle.setCenterX(lagerstatusText.getLayoutX() + lagerstatusText.getBoundsInLocal().getWidth() + lagerStatusCirkelSize + 10);
+
+                //set mouse clicked on, to switch to product view
+                EventHandler<MouseEvent> clicked = new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        App.setCurrentProduct(p);
+                        try {
+                            App.setLastSceneFxml("catelogView");
+                            App.setRoot("productInformation");
+                            System.out.println("\n\n TEST TEST TEST TEST TEST \n\n");
+                        } catch (Exception e) {
+                            System.out.println("Error in " + e.getMessage());;
+                        }
+                    }
+                };
+
+                EventHandler<MouseEvent> entered = new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        App.getStage().getScene().setCursor(Cursor.HAND);
+                    }
+                };
+
+                EventHandler<MouseEvent> exited = new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        App.getStage().getScene().setCursor(Cursor.DEFAULT);
+                    }
+                };
+
+                pane.getChildren().add(imgView);
+                pane.getChildren().add(nameText);
+                pane.getChildren().add(priceText);
+                pane.getChildren().add(lagerstatusText);
+                pane.getChildren().add(lagerstatusCircle);
+
+                /*pane.setOnMouseClicked(clicked);
+                pane.setOnMouseEntered(entered);
+                pane.setOnMouseExited(exited);*/
+                for (Node sp : pane.getChildren()) {
+                    sp.setOnMouseClicked(clicked);
+                    sp.setOnMouseEntered(entered);
+                    sp.setOnMouseExited(exited);
+                }
+
+                anchorPaneProducts.getChildren().add(pane);
+                if (column % panesPerRow == 0) {
+                    column = 0;
+                    row++;
+                }
+            }
+        }
+    }
+    
     @FXML
     void openProfile() throws Exception{
         App.setRoot("profileAdmin");
